310925933c49693c3fe937b9edfcff2d84a412d5
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-18 01:15:23 +0700

    Create ex10.php

---
+++ lesson_2/ex10.php
@@ -0,0 +1,8 @@
+<?php
+// Viết một chương trình PHP để loại bỏ khoảng trắng ở đầu và cuối chuỗi sử dụng hàm trim().
+function fTrim($variable) {
+    $trimmedVar = trim($variable);
+    echo "Chuỗi gốc: " . $variable . "<br>Thành chuỗi: " . $trimmedVar;
+}
+$string = "It's been a long day without you";
+fTrim($string);
