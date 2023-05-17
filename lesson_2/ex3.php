925e74999b11565fe5c7b84801284f32b4a771bf
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-17 21:36:14 +0700

    Update ex3.php

--- lesson_2/ex3.php
+++ lesson_2/ex3.php
@@ -1,5 +1,7 @@
 <?php
 // Viết một chương trình PHP để đảo ngược một chuỗi sử dụng hàm strrev().
-function fStrrev() {
-    return strrev("Hello World!");
+function fStrrev($variable) {
+    return strrev($variable);
 }
+
+echo "Chuỗi sau khi đảo: " . fStrrev("Toast to the ones that we lost on the way");
