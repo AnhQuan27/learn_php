e0922a01ded1d0c455e4b5bb9d2986a36fa2d14f
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-18 01:14:05 +0700

    Update ex1.php

--- lesson_2/ex1.php
+++ lesson_2/ex1.php
@@ -1,7 +1,7 @@
 <?php
     //Viết một chương trình PHP để đếm số ký tự trong một chuỗi sử dụng hàm strlen().
     function fStrlen($variable) {
-        return strlen($variable);
+        echo "Số ký tự: " . strlen($variable);
     }
 
-    echo "Số ký tự: " . fStrlen("Học lập trình");
+   fStrlen("Học lập trình");
