8d9be0e3a0840e302eb777318f0f33d9e9282a3f
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-19 00:38:38 +0700

    Update ex17.php

---
+++ lesson_2/ex17.php
@@ -0,0 +1,11 @@
+<?php
+// Viết một chương trình PHP để kiểm tra xem một chuỗi có chứa một chuỗi con khác không sử dụng hàm strstr().
+function fStrstr($var_1_17, $var_2_17) {
+    $return = strstr($var_1_17, $var_2_17);
+    echo $return ?
+    "Chuỗi \"$var_1_17\" không chứa chuỗi \"$var_2_17\" "
+    : 
+    "Chuỗi \"$var_1_17\" chứa chuỗi con \"$var_2_17\"";
+}
+
+fStrstr("Hello World!", "VietNam");
