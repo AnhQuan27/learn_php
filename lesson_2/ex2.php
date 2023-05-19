f003358488f8e796dfb9120dedbf961fc63369bd
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-19 22:58:40 +0700

    Update ex2.php

--- lesson_2/ex2.php
+++ lesson_2/ex2.php
@@ -1,7 +1,7 @@
 <?php
 // Viết một chương trình PHP để đếm số từ trong một chuỗi sử dụng hàm str_word_count().
 function fWordCount($variable) {
-    echo "Số từ: " . str_word_count($variable);
+    return str_word_count($variable);
 }
 
-fWordCount("Anything that can go wrong, will go wrong");
+echo "Số từ: " . fWordCount("Anything that can go wrong, will go wrong");
