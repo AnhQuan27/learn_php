6959180acc76d074c709c04df42b2cf02a5eef19
Quân <94169940+AnhQuan27@users.noreply.github.com>
2023-05-24 12:41:46 +0700

    Update ex16.php

--- lesson_2/ex16.php
+++ lesson_2/ex16.php
@@ -1,11 +1,12 @@
 <?php
 // Viết một chương trình PHP để kiểm tra xem một chuỗi có kết thúc bằng một chuỗi con khác không, sử dụng hàm strrchr().
-function fStrrchr($var_16, $var_2_16) {
-    $return = strrchr($var_16, $var_2_16);
-    echo $return ?
-    "Chuỗi không kết thúc bằng \"$var_2_16\" "
-    : 
-    "Chuỗi kết thúc bằng \"$var_2_16\"";
+function fStrrchr($var_1_16, $var_2_16) {
+    $return = strrchr($var_1_16, $var_2_16);
+    if($return !== false && $return === substr($var_1_16, -strlen($var_2_16))){     // Lấy phần cuối của chuỗi $var_1 có độ dài bằng với độ dài của chuỗi con $var_2
+        echo "Chuỗi \"$var_1_16\" kết thúc bằng chuỗi con \"$var_2_16\"";
+    } else {
+        echo "Chuỗi \"$var_1_16\" không kết thúc bằng chuỗi con \"$var_2_16\"";
+    }
 }
-fStrrchr("Hear my thoughts in every note", "every");
+fStrrchr("Hear my thoughts in every note","note");
 
